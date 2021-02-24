e5d0dd52b1b9c4dec1615993f0175a7948e5d6df
jesteena17 <jesteena17@gmail.com>
2021-02-24 17:07:47 +0530

    route param added

--- src/app/printproducts/item/item.component.ts
+++ src/app/printproducts/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { SharedService } from '../../shared.service'
 
@@ -9,16 +10,17 @@ import { SharedService } from '../../shared.service'
 })
 export class ItemComponent implements OnInit {
   sharedService : SharedService;
-
+  product_id:number;
   // productUpdated = new Subject<void>();
   product : any;
 
-  constructor(sharedService : SharedService) { 
+  constructor(sharedService : SharedService,private actRoute: ActivatedRoute) { 
     this.sharedService = sharedService;
+    this.product_id = this.actRoute.snapshot.params.id;
   }
 
   ngOnInit(): void {
-    this.sharedService.getProducts(2).then(data=>{
+    this.sharedService.getProducts(this.product_id).then(data=>{
     console.log(data);
     this.product = data
   })
